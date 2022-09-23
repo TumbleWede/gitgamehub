da4e6034dbd9e51b623638fcaf677eb5d3f4581a
TumbleWede <craftoriumz@gmail.com>
2022-09-23 08:14:07 -0500

    Update home.js

--- home.js
+++ home.js
@@ -1,6 +1,6 @@
 const home = document.createElement("img");
 home.src = "https://tumblewede.github.io/assets/favicon.png";
 home.removeAttribute("style");
-home.style = "width: 60px; display: block; margin: 8px";
+home.style = "width: 60px; display: block; margin: 8px; cursor: pointer;";
 home.onclick = () => {window.location.replace("https://tumblewede.github.io")}
 document.body.appendChild(home);
