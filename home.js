61ad1c39f6f2e5fef65e709d9fde9fbcb8135fd6
TumbleWede <craftoriumz@gmail.com>
2022-09-23 06:44:15 -0500

    added a button to get back to the home page

---
+++ home.js
@@ -0,0 +1,5 @@
+const home = document.createElement("img");
+home.src = "https://tumblewede.github.io/assets/favicon.png";
+home.style = "width:60px";
+home.onclick = () => {window.location.replace("https://tumblewede.github.io")}
+document.body.appendChild(home);
