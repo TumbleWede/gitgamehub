404121c00f9a49c35895fc37d4cc6823e28c78fb
TumbleWede <craftoriumz@gmail.com>
2022-10-24 08:14:24 -0500

    changed website domain

--- home.js
+++ home.js
@@ -1,6 +1,6 @@
 const home = document.createElement("img");
-home.src = "https://tumblewede.github.io/assets/home.png";
+home.src = "https://gitgamehub.vercel.app/assets/home.png";
 home.removeAttribute("style");
 home.style = "width: 60px; display: block; margin: 8px; cursor: pointer; image-rendering: auto;";
-home.onclick = () => {window.location.replace("https://tumblewede.github.io")}
+home.onclick = () => {window.location.replace("https://gitgamehub.vercel.app")}
 document.body.appendChild(home);
