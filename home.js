dfdda85fa7de6d6177262bb8a0e30cb045c2a810
TumbleWede <craftoriumz@gmail.com>
2022-11-11 06:24:57 -0600

    switched from vercel to netlify

--- home.js
+++ home.js
@@ -1,6 +1,6 @@
 const home = document.createElement("img");
-home.src = "https://gitgamehub.vercel.app/assets/home.png";
+home.src = "https://gitgamehub.netlify.app/assets/home.png";
 home.removeAttribute("style");
 home.style = "width: 60px; display: block; margin: 8px; cursor: pointer; image-rendering: auto;";
-home.onclick = () => {window.location.replace("https://gitgamehub.vercel.app")}
+home.onclick = () => {window.location.replace("https://gitgamehub.netlify.app")}
 document.body.appendChild(home);
