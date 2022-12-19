e3a3c162405e38dc0e3bc325f85d7f3c9652c72e
TumbleWede <craftoriumz@gmail.com>
2022-12-19 17:38:02 -0600

    Changed directory URLs

--- home.js
+++ home.js
@@ -1,6 +1,6 @@
 const home = document.createElement("img");
-home.src = "https://gitgamehub.netlify.app/assets/home.png";
+home.src = "/assets/home.png";
 home.removeAttribute("style");
-home.style = "width: 60px; display: block; margin: 8px; cursor: pointer; image-rendering: auto;";
-home.onclick = () => {window.location.replace("https://gitgamehub.netlify.app")}
+home.style = "width: 60px; position: absolute; display: block; margin: 8px; cursor: pointer; image-rendering: auto;";
+home.onclick = () => {window.location.replace("/")}
 document.body.appendChild(home);
