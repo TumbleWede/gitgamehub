dcac1d36a5798535604a5656739ee62cbdaa4650
TumbleWede <craftoriumz@gmail.com>
2023-01-04 18:41:27 -0600

    chess

--- chess/script.js
+++ chess/script.js
@@ -1,8 +1,8 @@
 const game = document.getElementById("game");
 const board = document.getElementById("board");
 const ctx = board.getContext("2d");
-board.width = 800;
-board.height = 800;
+board.width = 1600;
+board.height = 1600;
 
 updateWindow();
 window.onresize = updateWindow;
@@ -17,24 +17,78 @@ function updateWindow() {
 	}
 }
 
-const grid = [];
+let currentMove = 0; // 0 = White; 1 = Black
+let letters = ["a", "b", "c", "d", "e", "f", "g", "h"]
 
 for (let i = 0; i < 8; i++) {
-	grid[i] = [];
-
 	for (let j = 0; j < 8; j++) {
-		grid[i][j] = null
-		ctx.fillStyle = (i + j) % 2 == 0 ? "#ffebcc" : "#ad884c";
-		ctx.fillRect(i * 100, j * 100, 100, 100);
+		ctx.fillStyle = (i + j) % 2 == 1 ? "#ffebcc" : "#ad884c";
+		ctx.fillRect(i * 200, 1400 - j * 200, 200, 200);
 	}
+
+	ctx.textAlign = "right";
+	ctx.textBaseline = "top";
+	ctx.font = "40px Verdana";
+	ctx.fillText(i + 1, 40, 1420 - i * 200);
+
+	ctx.textAlign = "left";
+	ctx.textBaseline = "bottom";
+	ctx.fillText(letters[i], i * 200 + 160, 1580);
 }
 
-function createPiece(id, x, y, width, height) {
+function createPiece(id, x, y, x2, y2) {
 	const img = document.createElement("img");
-	img.id = id;
-	img.src = `assets/pieces.svg#svgView(viewBox(${x}, ${y}, ${width}, ${height}))`;
-	img.style.width = "12.5%";
-	img.style.height = "12.5%";
+	img.src = `assets/pieces.svg#svgView(viewBox(${x * 45}, ${y * 45}, 45, 45))`;
+	img.style.width = "10%";
+	img.style.height = "10%";
 	img.style.position = "absolute";
+	img.style.left = (x2 + 1) * 10 + "%";
+	img.style.top = 90 - (y2 + 1) * 10 + "%";
+	img.style.position = "absolute";
+	img.id = id;
+	img.dataset.x = x2;
+	img.dataset.y = y2;
 	game.appendChild(img);
+	return img;
 }
+
+const pieces = {
+	white: [
+		createPiece("Pawn", 5, 0, 0, 1),
+		createPiece("Pawn", 5, 0, 1, 1),
+		createPiece("Pawn", 5, 0, 2, 1),
+		createPiece("Pawn", 5, 0, 3, 1),
+		createPiece("Pawn", 5, 0, 4, 1),
+		createPiece("Pawn", 5, 0, 5, 1),
+		createPiece("Pawn", 5, 0, 6, 1),
+		createPiece("Pawn", 5, 0, 7, 1),
+
+		createPiece("Rook",   4, 0, 0, 0),
+		createPiece("Bishop", 2, 0, 1, 0),
+		createPiece("Knight", 3, 0, 2, 0),
+		createPiece("Queen",  1, 0, 3, 0),
+		createPiece("King",   0, 0, 4, 0),
+		createPiece("Knight", 3, 0, 5, 0),
+		createPiece("Bishop", 2, 0, 6, 0),
+		createPiece("Rook",   4, 0, 7, 0),
+	],
+	black: [
+		createPiece("Pawn", 5, 1, 0, 6),
+		createPiece("Pawn", 5, 1, 1, 6),
+		createPiece("Pawn", 5, 1, 2, 6),
+		createPiece("Pawn", 5, 1, 3, 6),
+		createPiece("Pawn", 5, 1, 4, 6),
+		createPiece("Pawn", 5, 1, 5, 6),
+		createPiece("Pawn", 5, 1, 6, 6),
+		createPiece("Pawn", 5, 1, 7, 6),
+
+		createPiece("Rook",   4, 1, 0, 7),
+		createPiece("Bishop", 2, 1, 1, 7),
+		createPiece("Knight", 3, 1, 2, 7),
+		createPiece("Queen",  1, 1, 3, 7),
+		createPiece("King",   0, 1, 4, 7),
+		createPiece("Knight", 3, 1, 5, 7),
+		createPiece("Bishop", 2, 1, 6, 7),
+		createPiece("Rook",   4, 1, 7, 7),
+	]
+};
